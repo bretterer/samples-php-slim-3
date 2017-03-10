d3b449ac5d21fd70a1f1189a4e1be18e0148db3c
Brian Retterer <brian.retterer@okta.com>
2017-03-10 10:15:44 -0500

    :arrow_up: Updates @okta/samples-generator 1.14.0
    OKTA-118792
    <<<Jenkins Check-In of Tested SHA: ebabe193711087c9e8c5e441a66ce388664d2433 for ci-admin@okta.com>>>
    Artifact: samples-php-slim-3

---
+++ src/public/index.php
@@ -0,0 +1,52 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+	$view = new \Slim\Views\Mustache([
+		'cache' => __DIR__ . '/../storage/cache/mustache',
+		'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
+		'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates/partials')
+	]);
+
+	return $view;
+};
+
+$container['logger'] = function($c) {
+	$logger = new \Monolog\Logger('my_logger');
+	$file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+	$logger->pushHandler($file_handler);
+	return $logger;
+};
+
+
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+$app->run();
---
+++ src/src/routes.php
@@ -0,0 +1,201 @@
+<?php
+// Routes
+
+use Symfony\Component\HttpFoundation\Cookie;
+
+$app->get('/', function ($request, $response, $args) {
+    $test = true;
+    return $this->view->render($response, 'overview.mustache', $args);
+});
+
+
+$app->get('/authorization-code/login-redirect', function ($request, $response, $args ) {
+    $args =  ['config' => json_decode(file_get_contents(__DIR__ . '/../../.samples.config.json'))->oktaSample];
+    return $this->view->render($response, 'login-redirect.mustache', $args );
+});
+
+$app->get('/authorization-code/login-custom', function ($request, $response, $args ) {
+    $args =  ['config' => json_decode(file_get_contents(__DIR__ . '/../../.samples.config.json'))->oktaSample];
+    return $this->view->render($response, 'login-custom.mustache', $args );
+});
+
+$app->get('/authorization-code/callback', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args ) {
+    $config = json_decode(file_get_contents(__DIR__ . '/../../.samples.config.json'))->oktaSample;
+	$cachedJwks = [];
+
+    if (! $request->getCookieParam('okta-oauth-nonce') || ! $request->getCookieParam('okta-oauth-state') ) {
+        return $response->withStatus(401);
+    }
+
+    if ( $request->getCookieParam('okta-oauth-state') != $request->getQueryParam('state')) {
+        return $response->withStatus(401);
+    }
+
+    if ( ! $request->getQueryParam('code') ) {
+        return $response->withStatus(401);
+    }
+
+	$authHeaderSecret = base64_encode($config->oidc->clientId . ':' . $config->oidc->clientSecret);
+
+    $query = http_build_query([
+    	'grant_type' => 'authorization_code',
+	    'code' => $request->getQueryParam('code'),
+	    'redirect_uri' => $config->oidc->redirectUri
+    ]);
+
+    $headers = [
+	    'Authorization: Basic ' . $authHeaderSecret,
+	    'Accept: application/json',
+	    'Content-Type: application/x-www-form-urlencoded',
+	    'Connection: close',
+        'Content-Length: 0'
+    ];
+
+    $url = $config->oidc->oktaUrl . '/oauth2/v1/token?' . $query;
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_POST, 1);
+
+
+	$output = curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	if(curl_error($ch))
+	{
+		$httpcode = 500;
+	}
+
+	$decodedOutput = json_decode($output);
+
+
+	try {
+		$tokenParts = explode('.',$decodedOutput->id_token);
+		if (3 !== count($tokenParts) || ( 3 === count($tokenParts) && $tokenParts[2] == '' ) ) {
+			return $response->withStatus(401);
+		}
+
+		$decoded = JOSE_JWT::decode($decodedOutput->id_token);
+
+		$kid = $decoded->header['kid'];
+
+		curl_close($ch);
+
+		$jwk = null;
+
+		if(file_exists(__DIR__ . '/../../src/storage/cache/' . $kid)) {
+			$jwk = file_get_contents(__DIR__ . '/../../src/storage/cache/' . $kid);
+			$jwk = unserialize($jwk);
+		}
+		else {
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $config->oidc->oktaUrl . '/oauth2/v1/keys');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+
+			$output = curl_exec($ch);
+
+			curl_close($ch);
+
+			$output = json_decode($output);
+
+			foreach ($output->keys as $key) {
+				// poormans cache
+				$s = serialize($key);
+				file_put_contents(__DIR__ . '/../../src/storage/cache/' . $key->kid, $s);
+
+				$cachedJwks[$key->kid] = $key;
+				if ($key->kid == $kid) {
+					$jwk = $key;
+				}
+			}
+		}
+
+		if( null === $jwk ) {
+			return $response->withStatus(401);
+		}
+
+		try {
+			$key = JOSE_JWK::decode((array)$jwk); # => phpseclib\Crypt\RSA instance
+
+			$jws = JOSE_JWT::decode($decoded);
+
+			if($jws->header['alg'] != $jwk->alg) {
+				return $response->withStatus(401);
+			}
+			$res = $jws->verify($key, $jwk->alg);
+
+			if($res->claims['nonce'] != $request->getCookieParam('okta-oauth-nonce')) {
+				return $response->withStatus(401);
+			}
+
+			if($res->claims['iss'] != $config->oidc->oktaUrl) {
+				return $response->withStatus(401);
+			}
+
+			if($res->claims['aud'] != $config->oidc->clientId) {
+				return $response->withStatus(401);
+			}
+
+			if($res->claims['exp'] < time()-300) {
+				return $response->withStatus(401);
+			}
+
+			if($res->claims['iat'] > time()+300) {
+				return $response->withStatus(401);
+			}
+
+		} catch (\Exception $e) {
+			return $response->withStatus(401);
+		}
+
+
+	} catch (\Gamegos\JWS\Exception\MalformedSignatureException $e) {
+		return $response->withStatus(401);
+	} catch (\Gamegos\JWS\Exception\InvalidSignatureException $e) {
+		return $response->withStatus(401);
+	} catch (\Exception $e) {
+		return $response->withStatus(500);
+	}
+
+    $userData = [
+        'email' => $res->claims['email'],
+        'claims' => $res->claims
+    ];
+
+
+    $userCookie = new Cookie('userData', serialize($userData), time() + 30, '/', 'localhost', false, false);
+
+    return $response->withAddedHeader('Set-Cookie', $userCookie)->withRedirect('/authorization-code/profile');
+
+
+
+});
+
+
+$app->get('/authorization-code/profile', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args ) {
+    if(!isset($_COOKIE['userData'])) {
+        return $response->withRedirect('/');
+    }
+
+    $userInfo = unserialize($_COOKIE['userData']);
+    $args = [
+        'user' => [
+            'email' => $userInfo['email'],
+            'claims' => $userInfo['claims']
+        ],
+        'config' => json_decode(file_get_contents(__DIR__ . '/../../.samples.config.json'))->oktaSample
+    ];
+    return $this->view->render($response, 'profile.mustache', $args );
+});
+
+$app->get('/authorization-code/logout', function ($request, \Slim\Http\Response $response, $args) {
+    $userCookie = new Cookie('userData', 'EXPIRED', 1, '/', 'localhost', false, false);
+
+    return $response->withAddedHeader('Set-Cookie', $userCookie)->withRedirect('/');
+
+});
